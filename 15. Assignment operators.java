a59cf6e2403d00423e5d53842219899834b85376
sanjana-reddy1652 <81432794+sanjana-reddy1652@users.noreply.github.com>
2021-06-14 11:13:03 +0530

    Create 15. Assignment operators.java

---
+++ 15. Assignment operators.java
@@ -0,0 +1,13 @@
+//Assignment operators
+package Pack1;
+public class Assignment {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+		int c = 20;			 
+		c += b;  
+	  System.out.println(c);	
+		c -= b;  
+		System.out.println(c);			 
+	}
+}
