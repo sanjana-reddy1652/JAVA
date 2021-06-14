7e5fd3c6a8b42ecf7a065888a94eeb8cde692bef
sanjana-reddy1652 <81432794+sanjana-reddy1652@users.noreply.github.com>
2021-06-14 10:32:12 +0530

    Create 2. Sum of 2 numbers.java

---
+++ 2. Sum of 2 numbers.java
@@ -0,0 +1,12 @@
+//sum of 2 numbers
+
+package Pack;
+public class Sum
+{
+	public static void main(String[] args) 
+	{
+		int a = 10, b = 20;
+		int c = a+b;
+		System.out.println("Sum = "+c);
+	}
+}
