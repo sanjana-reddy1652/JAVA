087c3096c12292b803fd50e0657478c46ffe30ac
sanjana-reddy1652 <81432794+sanjana-reddy1652@users.noreply.github.com>
2021-06-14 10:40:47 +0530

    Create 7. Explicit typecasting.java

---
+++ 7. Explicit typecasting.java
@@ -0,0 +1,11 @@
+//Explicit typecasting
+
+package Pack;
+public class Type_casting {
+	public static void main(String[] args) {	
+		long l = 50;
+		int i = (int)l;
+		System.out.println("int   = "+i);
+		System.out.println("long  = "+l);
+	}
+}
