722ef0a91c34c9783d8b91e09cdc33ce7bba3e14
sanjana-reddy1652 <81432794+sanjana-reddy1652@users.noreply.github.com>
2021-06-14 11:32:16 +0530

    Create 21. even or odd.java

---
+++ 21. even or odd.java
@@ -0,0 +1,14 @@
+//even or odd
+package cond;
+import java.util.*;
+public class If_Else {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number :");
+		int n = sc.nextInt(); 
+		if(n % 2 == 0)
+			System.out.println("even number");
+		else
+			System.out.println("odd number");
+	}
+}
