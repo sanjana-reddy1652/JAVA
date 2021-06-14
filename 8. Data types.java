2e4f7ff64372cc08e3c13a401575367aa831bc7c
sanjana-reddy1652 <81432794+sanjana-reddy1652@users.noreply.github.com>
2021-06-14 10:42:08 +0530

    Create 8. Data types.java

---
+++ 8. Data types.java
@@ -0,0 +1,23 @@
+// data types 
+
+package Pack;
+public class Data_types {
+	public static void main(String[] args) {
+		byte a = 110;
+		float f = 10.6f;
+		int i = 10;	
+		double d = 234.567;
+		boolean b = true;
+		char x = 'a';
+		short s = 155;
+		long l = 12345L;
+		System.out.println("int   = "+i);
+		System.out.println("float  = "+f);
+		System.out.println("double    = "+d);
+		System.out.println("boolean  = "+b);
+		System.out.println("char  = "+x);
+		System.out.println("byte   = "+a);
+		System.out.println("short   = "+s);
+		System.out.println("long   = "+l);
+	}
+}
