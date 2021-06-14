76d00c7b042d902aa298c7d27bf3e6986140fc7a
sanjana-reddy1652 <81432794+sanjana-reddy1652@users.noreply.github.com>
2021-06-14 10:34:22 +0530

    Create 3. Home address.java

---
+++ 3. Home address.java
@@ -0,0 +1,17 @@
+//display home address
+
+package Pack;
+public class Home_Addr 
+{
+	public static void main(String[] args) {
+		String name = "suresh";
+		String dno = "11-30";
+		String city = "Kakinada";
+		String state = "Andhra Pradesh";
+		System.out.println("Address :");
+		System.out.println("Name = "+name);
+		System.out.println("DNO = "+dno);
+		System.out.println("City = "+city);
+		System.out.println("State = "+state);
+	}
+}
