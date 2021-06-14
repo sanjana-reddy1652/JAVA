7828837c6f818f356a4efb3f4810c19b8267b429
sanjana-reddy1652 <81432794+sanjana-reddy1652@users.noreply.github.com>
2021-06-14 10:44:35 +0530

    Create 10. student details.java

---
+++ 10. student details.java
@@ -0,0 +1,18 @@
+//student details
+package Pack1;
+import java.util.*;
+public class Details {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter sno: ");
+		byte sno = sc.nextByte();
+		System.out.println("Enter sname: ");
+		String sna = sc.next();
+		System.out.println("Enter marks: ");
+		int marks = sc.nextInt();
+		System.out.println("Enter fee: ");
+		float fee = sc.nextFloat();	
+		System.out.println("Student Details: ");
+		System.out.println("Sno = "+sno+"\nsname = "+sna+"\nmarks = "+marks+"\nfee = "+fee);
+	}
+}
