ebba2e76d34036b0a39f105940343c98ffa1309f
sanjana-reddy1652 <81432794+sanjana-reddy1652@users.noreply.github.com>
2021-06-14 10:45:25 +0530

    Create 11. accepting sentence.java

---
+++ 11. accepting sentence.java
@@ -0,0 +1,12 @@
+// accept sentence
+
+package Pack1;
+import java.util.*;
+public class Sentence {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter sentence: ");
+		String s = sc.nextLine();
+		System.out.println("Sentence = " + s);
+	}
+}
