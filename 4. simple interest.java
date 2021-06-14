0ec10c1e4898ef77a730dee256015f9148e9ab15
sanjana-reddy1652 <81432794+sanjana-reddy1652@users.noreply.github.com>
2021-06-14 10:35:41 +0530

    Create 4. simple interest.java

---
+++ 4. simple interest.java
@@ -0,0 +1,12 @@
+//simple interest
+
+package Pack;
+public class Simple_interst {
+	public static void main(String[] args) {
+		int p = 12000;
+		int r = 3;
+		int n = 2;
+		int si = (p*n*r)/100;
+		System.out.println("Simple Interest = "+si);
+	}
+}
