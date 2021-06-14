3c8d5d767f01c479c8e38025151506be25fa83a1
sanjana-reddy1652 <81432794+sanjana-reddy1652@users.noreply.github.com>
2021-06-14 11:24:56 +0530

    Create 18. shift operators.java

---
+++ 18. shift operators.java
@@ -0,0 +1,13 @@
+// shift operators
+package Pack1;
+public class Shift {
+	public static void main(String[] args) {		
+		System.out.println(10<<2); 
+		System.out.println(10<<3); 
+		System.out.println(20<<2); 
+		System.out.println(15<<4); 
+    System.out.println(10>>2);
+    System.out.println(20>>2);
+		System.out.println(20>>>2);
+    
+	}}
