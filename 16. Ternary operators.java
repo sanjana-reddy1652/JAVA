22e3ad9bd6154939da486496262b45d814e3d710
sanjana-reddy1652 <81432794+sanjana-reddy1652@users.noreply.github.com>
2021-06-14 11:21:18 +0530

    Create 16. Ternary operators.java

---
+++ 16. Ternary operators.java
@@ -0,0 +1,12 @@
+// ternary operators
+
+//(condition?truevalue:falsevalue)
+
+package Pack1;
+public class Ternary {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;			 
+		int c =( (a<b) ? a : b );
+		System.out.println(c);			 
+	} }
