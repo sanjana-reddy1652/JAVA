9b724fd064e0d2330a2019ad4aef49a7d7d96b98
sanjana-reddy1652 <81432794+sanjana-reddy1652@users.noreply.github.com>
2021-06-14 11:26:16 +0530

    Create 19. Bitwise operators.java

---
+++ 19. Bitwise operators.java
@@ -0,0 +1,11 @@
+// bitwise operators
+
+package Pack1;
+public class Bitwise {
+	public static void main(String[] args) {		
+		int a=10;
+		int b=19;
+		System.out.println(a | b); 
+    System.out.println(a & b); 
+		System.out.println(a ^ b); 
+	}}
