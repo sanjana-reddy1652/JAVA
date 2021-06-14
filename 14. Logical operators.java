648240fdfa7875ada4078b3980cdd50fa89adcd3
sanjana-reddy1652 <81432794+sanjana-reddy1652@users.noreply.github.com>
2021-06-14 11:09:59 +0530

    Create 14. Logical operators.java

---
+++ 14. Logical operators.java
@@ -0,0 +1,15 @@
+// Logical operators
+
+package Pack1;
+public class Logical {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+		int c = 20;
+		System.out.println(a<b && a<c); 
+    System.out.println(a>b && a<c); 
+		System.out.println(a<b || a<c); 
+		System.out.println(a<b || a>c); 
+		System.out.println(a>b || a<c); 
+	} 
+}
