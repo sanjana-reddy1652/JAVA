3ad7635f9447d7d78a049d84ec0623d67a5cb529
sanjana-reddy1652 <81432794+sanjana-reddy1652@users.noreply.github.com>
2021-06-14 10:38:02 +0530

    Create 5. (a) Implicit typecasting.java

---
+++ 5. (a) Implicit typecasting.java
@@ -0,0 +1,11 @@
+//Implicit typecasting
+
+package Pack1;
+public class Type_casting {
+	public static void main(String[] args) {
+		byte b = 10;
+		int i = b;
+		System.out.println("byte  = "+b);
+		System.out.println("int  = "+i);
+	}
+}
