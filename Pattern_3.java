a3c0256bd9e901bc49738ecc264de0ebeb38b021
sanjana-reddy1652 <81432794+sanjana-reddy1652@users.noreply.github.com>
2021-06-15 09:22:38 +0530

    Add files via upload

---
+++ Pattern_3.java
@@ -0,0 +1,20 @@
+/*Pattern_3
+
+12345
+12345
+12345
+12345
+12345
+*/
+package Patterns;
+public class Pattern_3 {
+	public static void main(String[] args) {
+		for(int i = 1; i<=5;i++) 
+		{
+		for (int j =1; j<=5; j++) 
+		{
+			System.out.print(j);
+		}
+		System.out.println();
+		}
+	} }
---
+++ Pattern_5.java
@@ -0,0 +1,19 @@
+/* Pattern_5
+54321
+54321
+54321
+54321
+54321
+*/
+package Patterns;
+public class Pattern_5 {
+	public static void main(String[] args) {
+		for(int i = 1; i<=5;i++) 
+		{
+		for (int j =5; j>=1; j--) 
+		{
+			System.out.print(j);
+		}
+		System.out.println();
+		}
+	} }
