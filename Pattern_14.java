57433fb0f42425451df7bc8269cece7f168e99de
sanjana-reddy1652 <81432794+sanjana-reddy1652@users.noreply.github.com>
2021-06-23 17:42:05 +0530

    Create Pattern_14.java

---
+++ Pattern_14.java
@@ -0,0 +1,21 @@
+/* Pattern_14
+1
+22
+333
+4444
+55555
+*/
+package Patterns;
+public class Pattern_14 {
+	public static void main(String[] args) {
+		
+		for(int i = 1; i <= 5; i++)  
+		{
+		for (int j=1; j<=i ;j++) 
+		{
+			System.out.print(i);			
+		}
+		System.out.println();
+		}
+	} 
+}
