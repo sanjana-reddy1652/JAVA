077384b1f1248dd34b1dcae968a419b21cf0b3c6
sanjana-reddy1652 <81432794+sanjana-reddy1652@users.noreply.github.com>
2021-06-28 08:08:58 +0530

    Create Pattern_18.java

---
+++ Pattern_18.java
@@ -0,0 +1,21 @@
+/*Pattern_18
+5
+45
+345
+2345
+12345
+*/
+package Patterns;
+public class Pattern_18 {
+	public static void main(String[] args) {
+		
+		for(int i = 5; i >= 1; i--) 
+		{
+		for (int j=i; j <= 5 ;j++) 
+		{
+			System.out.print(j);			
+		}
+		System.out.println();
+		}
+	} 
+  }
